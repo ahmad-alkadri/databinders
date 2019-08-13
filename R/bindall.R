ac92b9c08d488abcb496e9f2500ec0207fa3835c
Ahmad Alkadri <ahmad.alkadri@outlook.com>
2019-08-13 13:19:44 +0200

    Updated the examples - CSV and ODS files available

--- R/bindall.R
+++ R/bindall.R
@@ -15,35 +15,43 @@
 #' files of the requested type in the repository.
 #'
 #' @examples
+#'
+#' download.file(url = "https://github.com/ahmad-alkadri/databinders/raw/master/Examples.zip",
+#'               destfile = "Examples.zip")
+#'
+#' unzip(zipfile = "Examples.zip")
+#'
 #' df <- bindall(repo = "Examples/XLSX/", type = "xlsx")
 
 bindall <- function(repo = getwd(), type = "xlsx"){
+
+  # obtain the current directory
+  homedir = getwd()
+
+  # setting the directory into the target repo
   setwd(repo)
 
-  file_list <- list.files(pattern = paste(".",type=".xlsx",sep=""))
+  file_list <- list.files()
 
   for (file in file_list){
 
     # if the merged dataset doesn't exist, create it
-    if (!exists("df")){
-
-      df <- rio::import(file)
-
+    if (!exists("dataset")){
+      dataset <- rio::import(file)
     }
 
     # if the merged dataset does exist, append to it
     if (exists("dataset")){
-
-      tmp_df <- rio::import(file)
-
-      df <- rbind(df, tmp_df)
-
-      rm(tmp_df)
-
+      tmp_dataset <-rio::import(file)
+      dataset<-rbind(dataset, tmp_dataset)
+      rm(tmp_dataset)
     }
 
   }
 
-  return(df)
+  # go back to the home directory
+  setwd(homedir)
+
+  return(dataset)
 
 }
